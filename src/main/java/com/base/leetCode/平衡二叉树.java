8edff17243b6630db5c8ee3184c2af8fde25b8ae
王春 <itchun@163.com>
2022-07-07 21:49:21 +0800

    提交

---
+++ src/main/java/com/base/leetCode/平衡二叉树.java
@@ -0,0 +1,41 @@
+package com.base.leetCode;
+
+import com.base.leetCode.entity.TreeNode;
+
+public class 平衡二叉树 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        TreeNode left = root.left;
+        int left_ = 0;
+        while (left != null) {
+            left_++;
+            left = left.left;
+        }
+        TreeNode right = root.right;
+        int right_ = 0;
+        while (right != null) {
+            right_++;
+            right = right.right;
+        }
+        if ((left_ - right_) > 1 || (right_ - left_) > 1) {
+            return false;
+        } else {
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
+
+    public int maxDepth(TreeNode root){
+        if(root == null) return 0;
+        int level = 1;
+        int left  = maxDepth(root.left);
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        
+    }
+
+}
